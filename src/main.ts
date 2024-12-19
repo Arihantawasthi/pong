c43cf0994f663d6171b616b63989d7d43d6222d6
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-12-19 20:06:35 +0530

    feat: created game field

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,61 @@
-const canvas: HTMLCanvasElement | null = document.querySelector("#game");
-if (!canvas) {
-    throw new Error("Canvas element not found");
-}
+class PongTable {
+    private canvas: HTMLCanvasElement
+    private ctx: CanvasRenderingContext2D
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+    constructor(canvasId: string) {
+        if (!canvasId.startsWith("#")) {
+            throw new Error("Canvas Id should start with '#'");
+        }
+        this.canvas = document.querySelector(`${canvasId}`) as HTMLCanvasElement;
+        if (!this.canvas) {
+            throw new Error(`Canvas with ${canvasId} ID not found`);
+        }
 
-const ctx = canvas.getContext("2d")
-if (!ctx) {
-    throw new Error("Error getting the context from canvas");
-}
+        this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+        if (!this.ctx) {
+            throw new Error(`Context for canvas ${canvasId} not found`);
+        }
 
-let y = 0;
-let secondsPassed;
-let oldTimestamp: number = 0;
-let fps;
-let velocity = 50;
-let acceleration = 50;
-
-function ballDrop(timestamp: number) {
-    secondsPassed = Math.min((timestamp - oldTimestamp) / 1000, 0.1);
-    oldTimestamp = timestamp;
-    fps = Math.round(1 / secondsPassed)
-
-    if (!ctx || !canvas) {
-        throw new Error("Canvas or Context doesn't exist");
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
     }
-    ctx.clearRect(0, 0, window.innerHeight, window.innerWidth);
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, y, 10, 0, 2 * Math.PI);
-    ctx.fillStyle = "white";
-    ctx.fill();
-    ctx.lineWidth = 0;
-    ctx.stroke();
-    y += (velocity * secondsPassed);
-    velocity += (acceleration * secondsPassed);
 
-    window.requestAnimationFrame(ballDrop);
+    public drawField() {
+        this.ctx.fillStyle = "#bbb";
+        this.ctx.fillRect(0, (this.canvas.height / 2) - 4, this.canvas.width, 8);
+
+        this.ctx.beginPath();
+        this.ctx.arc((this.canvas.width / 2), (this.canvas.height / 2), 150, 0, 2 * Math.PI)
+        this.ctx.lineWidth = 8;
+        this.ctx.strokeStyle = "#bbb";
+        this.ctx.stroke();
+
+        this.ctx.beginPath();
+        this.ctx.moveTo((this.canvas.width / 2) - 300, this.canvas.height);
+        this.ctx.lineTo((this.canvas.width / 2) - 300, this.canvas.height - 150);
+        this.ctx.lineTo(((this.canvas.width / 2) - 300) + 600, this.canvas.height - 150);
+        this.ctx.lineTo(((this.canvas.width / 2) - 300) + 600, this.canvas.height);
+        this.ctx.stroke();
+
+        this.ctx.beginPath();
+        this.ctx.moveTo((this.canvas.width / 2) - 300, 0);
+        this.ctx.lineTo((this.canvas.width / 2) - 300, 150);
+        this.ctx.lineTo(((this.canvas.width / 2) - 300) + 600, 150);
+        this.ctx.lineTo(((this.canvas.width / 2) - 300) + 600, 0);
+        this.ctx.stroke();
+
+        this.drawBall();
+    }
+
+    public drawBall() {
+        this.ctx.beginPath();
+        this.ctx.arc((this.canvas.width / 2), this.canvas.height / 2, 12, 0, 2 * Math.PI);
+        this.ctx.fillStyle = "white";
+        this.ctx.fill();
+        this.ctx.lineWidth = 1;
+        this.ctx.stroke();
+    }
 }
 
-window.requestAnimationFrame(ballDrop);
+const pong = new PongTable("#game");
+pong.drawField();
